7f85f5246d8e6839aa354c7b2e20c5c3146ec48e
Julian French <ju.french@gmail.com>
2018-11-20 09:27:44 -0600

    Updates

---
+++ precache-manifest.6d20c51fb9f5b9bcae50d3cb1c36bd17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todolist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "15e18c78eb78a1dce817",
+    "url": "/react-todolist/static/js/runtime~main.15e18c78.js"
+  },
+  {
+    "revision": "731e6c3ae02da8ebd4ba",
+    "url": "/react-todolist/static/js/main.731e6c3a.chunk.js"
+  },
+  {
+    "revision": "a39875ff8d3e4943fe6d",
+    "url": "/react-todolist/static/js/1.a39875ff.chunk.js"
+  },
+  {
+    "revision": "731e6c3ae02da8ebd4ba",
+    "url": "/react-todolist/static/css/main.a9c9a50b.chunk.css"
+  },
+  {
+    "revision": "bff78289fc2def91ec8d710b0f1e33fc",
+    "url": "/react-todolist/index.html"
+  }
+];
